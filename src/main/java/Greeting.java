e02ea0aec610e982376d8f54597c6510c4b33478
Aqilalh <Aqilalhaidari@gmail.com>
2022-09-15 09:37:08 -0400

    doing homework

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -3,11 +3,13 @@ public class Greeting {
     //public static void String personName(String name) {
     public static void main (String[]args){
         //https://www.youtube.com/watch?v=bI7kQq9cjmQ
+        // create a scanner method
         Scanner scan = new Scanner(System.in);
         System.out.println("whats your name");
         String name = scan.nextLine();
         System.out.println(name);
-        if (name.equalsIgnoreCase("Alcia") || name.equalsIgnoreCase("Bob")) {
+        // you need a if stament that prints out hello alice ore Bob only and anybody else print out not welcome
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
             System.out.println("hello welcome " + name);
         } else{
             System.out.println("hello your not welcome");
