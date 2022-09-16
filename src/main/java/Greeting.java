ce503a4112331d63da3ef084b1932f32bcfba497
Aqilalh <Aqilalhaidari@gmail.com>
2022-09-16 14:09:49 -0400

    Finished Hw

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 public class Greeting {
     //public static void String personName(String name) {
-    public static void main (String[]args){
+    public static void main (String[]args) {
         //https://www.youtube.com/watch?v=bI7kQq9cjmQ
         // create a scanner method
         Scanner scan = new Scanner(System.in);
         System.out.println("whats your name");
         String name = scan.nextLine();
         System.out.println(name);
+        System.out.println("whats your name");
+        name = scan.nextLine();
         // you need a if stament that prints out hello alice ore Bob only and anybody else print out not welcome
-        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
-            System.out.println("hello welcome " + name);
-        } else{
-            System.out.println("hello your not welcome");
+        //for (int i = 0; i < name.length(); i++) {
+          while(true){
+            if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+                System.out.println("hello welcome " + name);
+
+            } else {
+                System.out.println("whats your name");
+                System.out.println(name = scan.nextLine());
+            }
+break;
         }
+
     }
-
-
         //   Hello names = new Hello();
        // System.out.println(names.helloName("Bob"));
         /* I need an if statment that when I did sout */
